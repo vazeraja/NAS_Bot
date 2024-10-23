4afa135d6996f8d2780fbfa6e6c05f218eefab9d
vazeraja <68131084+vazeraja@users.noreply.github.com>
2024-10-23 19:51:10 -0400

    init

---
+++ NAS_Bot/NAS_Bot/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+
+namespace NAS_Bot
+{
+    internal class JSONReader
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+
+        public async Task ReadJSON()
+        {
+            using (StreamReader sr = new StreamReader("config.json")) { }
+        }
+    }
+
+    internal sealed class JSONStructure
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+    }
+
+    internal class Program
+    {
+        private static DiscordClient client { get; set; }
+        private static CommandsNextExtension commands { get; set; }
+        
+        public static void Main(string[] args) { }
+    }
+}
